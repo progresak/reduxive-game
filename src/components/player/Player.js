7a0f566e05e19f376cecb06f9550c0b60335372e
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2019-06-13 23:46:51 +0200

    Revert player to "one-player" version

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,50 +1,21 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { connect } from 'react-redux';
 import playerWalk from './player_walk.png';
 import handleMovement from './movement';
 import handleControl from './control';
-import { createPlayer as CP } from '../../actions';
-import { BOMB_TIMEOUT, DEFAULT_BOMB_POWER, DEFAULT_MAXIMUM_BOMBS } from '../../config/constants';
-
-export const defaultPlayer = name => ({
-  name,
-  position: [0, 0],
-  spriteLocation: '0px Opx',
-  defaultDirection: 'EAST',
-  walkIndex: 0,
-  bombPower: DEFAULT_BOMB_POWER,
-  bombTimeout: BOMB_TIMEOUT,
-  maximumBombs: DEFAULT_MAXIMUM_BOMBS,
-  gameOver: false,
-  controls: {
-    up: 1,
-    down: 13,
-    left: 12,
-    right: 11,
-    bomb: 23,
-  },
-});
 
 const Player = (props) => {
-  const {
-    playingPlayers, createPlayer, name, gameOver,
-  } = props;
-  useEffect(() => {
-    createPlayer(defaultPlayer(name));
-  }, []);
-
-  if (gameOver || !playingPlayers.length) {
+  if (props.gameOver) {
     return null;
   }
 
   return (
     <div style={{
       position: 'absolute',
-      top: playingPlayers[0].position[1],
-      left: playingPlayers[0].position[0],
+      top: props.position[1],
+      left: props.position[0],
       backgroundImage: `url('${playerWalk}')`,
-      backgroundPosition: playingPlayers[0].spriteLocation,
+      backgroundPosition: props.spriteLocation,
       width: '40px',
       height: '40px',
     }}
@@ -52,23 +23,8 @@ const Player = (props) => {
   );
 };
 
-Player.propTypes = {
-  playingPlayers: PropTypes.any.isRequired,
-  createPlayer: PropTypes.func.isRequired,
-  name: PropTypes.string.isRequired,
-  gameOver: PropTypes.bool,
-};
-
-Player.defaultProps = {
-  gameOver: false,
-};
-
 const mapStateToProps = state => ({
-  // ...state.player,
-  playingPlayers: state.map.players,
+  ...state.player,
 });
 
-
-export default connect(
-  mapStateToProps, { createPlayer: CP },
-)(handleControl(handleMovement(Player)));
+export default connect(mapStateToProps)(handleControl(handleMovement(Player)));
