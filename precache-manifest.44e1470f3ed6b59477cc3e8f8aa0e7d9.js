eebb3baa94bc9c2bb186f52877f614d78bb182c5
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2020-01-10 07:32:35 +0100

    Initial build subtree commit

---
+++ precache-manifest.44e1470f3ed6b59477cc3e8f8aa0e7d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e0a15b3e8296a315bd3",
+    "url": "./static/css/main.bb635376.chunk.css"
+  },
+  {
+    "revision": "8e0a15b3e8296a315bd3",
+    "url": "./static/js/main.1d42ddf9.chunk.js"
+  },
+  {
+    "revision": "52dd09a350d833821d34",
+    "url": "./static/js/runtime~main.2a45a954.js"
+  },
+  {
+    "revision": "cce7c479ecb7bec9bc64",
+    "url": "./static/js/2.80a2354f.chunk.js"
+  },
+  {
+    "revision": "f323427373160cbb1f62",
+    "url": "./static/css/3.c2646931.chunk.css"
+  },
+  {
+    "revision": "f323427373160cbb1f62",
+    "url": "./static/js/3.ecb1cc74.chunk.js"
+  },
+  {
+    "revision": "afabfe52c83bf4b4873a79fed96526fb",
+    "url": "./static/media/bomb.afabfe52.svg"
+  },
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "./static/media/player_walk.9f19440f.png"
+  },
+  {
+    "revision": "ae903f63fdd54121f40f70d261c661e8",
+    "url": "./index.html"
+  }
+];
