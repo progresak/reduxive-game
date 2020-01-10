12b1fb4c5cf199c88ebed64d7fd65459b9b37d05
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2020-01-10 07:13:14 +0100

    added overload to indention to 4

--- src/components/ScoreBoard/index.js
+++ src/components/ScoreBoard/index.js
@@ -4,27 +4,27 @@ import { connect } from 'react-redux';
 import explosion from '../map/tiles/explosion.png';
 
 const ScoreBoard = ({ bombPower, dimension }) => (
-  <div className="scoreBoard">
-    <span>
-      <img style={{ height: '40px', margin: '5px' }} src={explosion} alt="Explosion" />
-      {bombPower}
-    </span>
+    <div className="scoreBoard">
+        <span>
+            <img style={{ height: '40px', margin: '5px' }} src={explosion} alt="Explosion" />
+            {bombPower}
+        </span>
 
-    <span>
+        <span>
       [
-      {`${dimension[0]}x${dimension[1]}`}
+            {`${dimension[0]}x${dimension[1]}`}
       ]
-    </span>
-  </div>
+        </span>
+    </div>
 );
 
 const mapStateToProps = (state) => {
-  const tiles = state.map.tiles;
+    const tiles = state.map.tiles;
 
-  return {
-    bombPower: state.player.bombPower,
-    dimension: (tiles.length) ? [tiles[0].length, tiles.length] : [],
-  };
+    return {
+        bombPower: state.player.bombPower,
+        dimension: (tiles.length) ? [tiles[0].length, tiles.length] : [],
+    };
 };
 
 export default connect(mapStateToProps)(ScoreBoard);
